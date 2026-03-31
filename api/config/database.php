c0f388602ca4f02bf7aa9157ae21e13731b63cdf
hiepquocchung5-blip <hiepquocchung5@gmail.com>
2026-03-31 23:52:11 +0630

    frontend allow

--- api/config/database.php
+++ api/config/database.php
@@ -44,7 +44,7 @@ try {
 }
 
 // Global CORS Headers specific to the API portal
-header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Origin: w.baganix.online"); // Allow only the web frontend
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
